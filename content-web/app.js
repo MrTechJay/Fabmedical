de61bd09e9028d75bc8160ab63ccd7bdc9c92012
MrTechJay <mr@techjay.de>
2022-03-17 13:13:18 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const request = require('request');
 
 const app = express();
+const appInsights = require("applicationinsights");
+appInsights.setup("b1afeb22-a3d7-45b7-ae9d-38ff7b3d331b");
+appInsights.start();
 
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
